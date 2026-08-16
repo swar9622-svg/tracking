b546c38885e7c0f3800a8c1b8af3e1e54d294750
swar9622-svg <swar9622@gmail.com>
2026-08-16 10:44:30 +0300

    Add files via upload

---
+++ sw2.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'attendance-app-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './html2pdf.bundle.min.js',
+  './html2canvas.min.js',
+  './jspdf.umd.min.js',
+  './xlsx.full.min.js',
+  './sw2.js'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
