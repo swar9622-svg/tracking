7e27caaa59eae05979438c373a65224b2a1f736e
swar9622-svg <swar9622@gmail.com>
2026-08-23 18:45:33 +0300

    Add files via upload

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-v20-71-safe-followup-pinch-zoom';
+const CACHE_NAME = 'attendance-app-v20-72-understanding-student-stats';
 const urlsToCache = [
   './',
   './index.html',
