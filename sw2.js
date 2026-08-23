bce06a5b3312d855c1a57d5325e2a4c03445e1a1
swar9622-svg <swar9622@gmail.com>
2026-08-23 15:34:08 +0300

    Add files via upload

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-v20-62-excellence-activity-priority';
+const CACHE_NAME = 'attendance-app-v20-63-excellence-summary-count';
 const urlsToCache = [
   './',
   './index.html',
