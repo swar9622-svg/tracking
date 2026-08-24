0f472bdd33f458f2807d02845734fc4254f351be
swar9622-svg <swar9622@gmail.com>
2026-08-24 06:46:28 +0300

    Add files via upload

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-v20-87-attendance-display-only-search';
+const CACHE_NAME = 'attendance-app-v20-88-understanding-display-only-search';
 const urlsToCache = [
   './',
   './index.html',
