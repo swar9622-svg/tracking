2ae57c74193c66a7c78ad499f3ef1d972f149334
swar9622-svg <swar9622@gmail.com>
2026-08-27 10:03:47 +0300

    Add files via upload

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-v21-54-multi-permission-swipe';
+const CACHE_NAME = 'attendance-app-v21-55-safari-longpress-swipe';
 const urlsToCache = [
   './',
   './index.html',
