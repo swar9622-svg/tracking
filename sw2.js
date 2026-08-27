62cf743d0550e20f9e193fe49dc35d1e5f02aaad
swar9622-svg <swar9622@gmail.com>
2026-08-27 11:09:53 +0300

    Add files via upload

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-v21-58-doubletap-permission';
+const CACHE_NAME = 'attendance-app-v21-59-auto-return-period-end';
 const urlsToCache = [
   './',
   './index.html',
